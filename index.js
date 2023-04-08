0da085d5d32a9417d40f57aa634d5077043e35b7
Nika Gogelia <gogelianika1@gmail.com>
2023-04-08 16:49:59 +0400

    add news section

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ navLinks.forEach((navLink) => {
     navLink.classList.add("active");
   });
 });
+
+// News Slider
+const newsProfiles = document.querySelector(".news-profiles");
+const newsCard = document.querySelectorAll(".news-card");
+const newsArrows = document.querySelectorAll(".news-arrows > img");
+
+let rangeValue = 0;
+let columnGap = 20;
+
+newsArrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    let scrollWidth = 0;
+
+    switch (arrow.id) {
+      case "prev":
+        if (rangeValue === 0) {
+          rangeValue = 0;
+        } else {
+          rangeValue--;
+        }
+        break;
+      case "next":
+        if (rangeValue === newsCard.length - 1) {
+          rangeValue = newsCard.length - 1;
+        } else {
+          rangeValue++;
+        }
+        break;
+      default:
+        break;
+    }
+    scrollWidth += (newsCard[0].clientWidth + columnGap) * rangeValue;
+    newsProfiles.style = `left: -${scrollWidth}px`;
+  });
+});
